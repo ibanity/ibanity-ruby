5eacbf6cbb22b3326cf499d0a40df52273002864
Christina Wolfe <christina.wolfe@ibanity.com>
2017-12-21 13:29:51 +0100

    Add method to delete a sandbox_user

--- lib/ibanity/api/sandbox_user.rb
+++ lib/ibanity/api/sandbox_user.rb
@@ -15,5 +15,10 @@ module Ibanity
       uri = Ibanity.api_schema["sandbox"]["users"].sub("{sandboxUserId}", id)
       find_by_uri(uri)
     end
+
+    def self.delete(id)
+      uri = Ibanity.api_schema["sandbox"]["users"].sub("{sandboxUserId}", id)
+      destroy_by_uri(uri)
+    end
   end
 end
