20b3816d37c0000baf1f1190e8b37a95711c3854
Christina Wolfe <christina.wolfe@ibanity.com>
2018-01-18 11:19:22 +0100

    Fix api_schema path for AccountInformationAccessRequest

--- lib/ibanity/api/account_information_access_request.rb
+++ lib/ibanity/api/account_information_access_request.rb
@@ -1,7 +1,7 @@
 module Ibanity
   class AccountInformationAccessRequest < Ibanity::BaseResource
     def self.create_for_financial_institution(financial_institution_id:, attributes:, access_token:)
-      path = Ibanity.api_schema["financialInstitution"]["accountInformationAccessRequests"]
+      path = Ibanity.api_schema["customer"]["financialInstitution"]["accountInformationAccessRequests"]
         .gsub("{financialInstitutionId}", financial_institution_id)
         .gsub("{accountInformationAccessRequestId}", "")
       uri = Ibanity.client.build_uri(path)
