d5b1c5ff9174bcbf9435adca5a17b61e9365c0bd
Christina Wolfe <christina.wolfe@ibanity.com>
2018-01-18 16:51:27 +0100

    Add ::all, ::find, and ::delete to SandboxTransaction

--- lib/ibanity/api/sandbox_transaction.rb
+++ lib/ibanity/api/sandbox_transaction.rb
@@ -9,5 +9,35 @@ module Ibanity
       uri = Ibanity.client.build_uri(path)
       create_by_uri(uri, "sandboxTransaction", attributes)
     end
+
+    def self.all(sandbox_user_id:, financial_institution_id:, sandbox_account_id:)
+      path = Ibanity.api_schema["sandbox"]["transactions"]
+        .gsub("{financialInstitutionId}", financial_institution_id)
+        .gsub("{sandboxUserId}", sandbox_user_id)
+        .gsub("{sandboxAccountId}", sandbox_account_id)
+        .gsub("{sandboxTransactionId}", "")
+      uri = Ibanity.client.build_uri(path)
+      all_by_uri(uri)
+    end
+
+    def self.find(id:, sandbox_user_id:, financial_institution_id:, sandbox_account_id:)
+      path = Ibanity.api_schema["sandbox"]["transactions"]
+        .gsub("{financialInstitutionId}", financial_institution_id)
+        .gsub("{sandboxUserId}", sandbox_user_id)
+        .gsub("{sandboxAccountId}", sandbox_account_id)
+        .gsub("{sandboxTransactionId}", id)
+      uri = Ibanity.client.build_uri(path)
+      find_by_uri(uri)
+    end
+
+    def self.delete(id:, sandbox_user_id:, financial_institution_id:, sandbox_account_id:)
+      path = Ibanity.api_schema["sandbox"]["transactions"]
+        .gsub("{financialInstitutionId}", financial_institution_id)
+        .gsub("{sandboxUserId}", sandbox_user_id)
+        .gsub("{sandboxAccountId}", sandbox_account_id)
+        .gsub("{sandboxTransactionId}", id)
+      uri = Ibanity.client.build_uri(path)
+      destroy_by_uri(uri)
+    end
   end
 end
