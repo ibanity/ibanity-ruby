6237ca7a78b7759e6310045e4f6b25741b3c3282
Christina Wolfe <christina.wolfe@ibanity.com>
2018-01-24 13:51:13 +0100

    Add account get method

--- lib/ibanity/api/account.rb
+++ lib/ibanity/api/account.rb
@@ -1,10 +1,17 @@
 module Ibanity
   class Account < Ibanity::BaseResource
-    def self.all_for_customer_in_financial_institution(financial_institution_id, customer_access_token)
+    def self.all_for_customer_in_financial_institution(financial_institution_id:, customer_access_token:)
       uri = Ibanity.api_schema["customer"]["financialInstitution"]["accounts"].sub(
         "{financialInstitutionId}", financial_institution_id
       ).sub("{accountId}", "")
       all_by_uri(uri, customer_access_token)
+    end
+
+    def self.get(id:, financial_institution_id:, customer_access_token:)
+      uri = Ibanity.api_schema["customer"]["financialInstitution"]["accounts"]
+        .sub("{financialInstitutionId}", financial_institution_id)
+        .sub("{accountId}", id)
+      find_by_uri(uri, customer_access_token)
     end
   end
 end
