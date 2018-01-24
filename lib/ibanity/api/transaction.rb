a596ef550bd25d82cd9ba0f5fb43ed3fcec7d55d
Christina Wolfe <christina.wolfe@ibanity.com>
2018-01-24 14:36:00 +0100

    Add transaction get and all methods

--- lib/ibanity/api/transaction.rb
+++ lib/ibanity/api/transaction.rb
@@ -7,5 +7,21 @@ module Ibanity
         .sub("{transactionId}", "")
       all_by_uri(uri)
     end
+
+    def self.all(financial_institution_id:, account_id:, customer_access_token:)
+      uri = Ibanity.api_schema["customer"]["financialInstitution"]["transactions"]
+        .sub("{financialInstitutionId}", financial_institution_id)
+        .sub("{accountId}", account_id)
+        .sub("{transactionId}", "")
+      all_by_uri(uri, customer_access_token)
+    end
+
+    def self.get(id:, financial_institution_id:, account_id:, customer_access_token:)
+      uri = Ibanity.api_schema["customer"]["financialInstitution"]["transactions"]
+        .sub("{financialInstitutionId}", financial_institution_id)
+        .sub("{accountId}", account_id)
+        .sub("{transactionId}", id)
+      find_by_uri(uri, customer_access_token)
+    end
   end
 end
