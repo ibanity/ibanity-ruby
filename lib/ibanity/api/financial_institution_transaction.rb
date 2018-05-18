1bc5cb906084f4f1ad00cecfe1b6d17aefa87e12
Christina Wolfe <christina.wolfe@ibanity.com>
2018-05-18 16:15:03 +0200

    PL-339 Rename Financial Institution resources

---
+++ lib/ibanity/api/financial_institution_transaction.rb
@@ -0,0 +1,43 @@
+module Ibanity
+  class FinancialInstitutionTransaction < Ibanity::BaseResource
+    def self.create(financial_institution_user_id:, financial_institution_id:, financial_institution_account_id:, idempotency_key: nil, **attributes)
+      path = Ibanity.api_schema["sandbox"]["transactions"]
+        .gsub("{financialInstitutionId}", financial_institution_id)
+        .gsub("{financialInstitutionUserId}", financial_institution_user_id)
+        .gsub("{financialInstitutionAccountId}", financial_institution_account_id)
+        .gsub("{sandboxTransactionId}", "")
+      uri = Ibanity.client.build_uri(path)
+      create_by_uri(uri: uri, resource_type: "sandboxTransaction", attributes: attributes, idempotency_key: idempotency_key)
+    end
+
+    def self.list(financial_institution_user_id:, financial_institution_id:, financial_institution_account_id:, **query_params)
+      path = Ibanity.api_schema["sandbox"]["transactions"]
+        .gsub("{financialInstitutionId}", financial_institution_id)
+        .gsub("{financialInstitutionUserId}", financial_institution_user_id)
+        .gsub("{financialInstitutionAccountId}", financial_institution_account_id)
+        .gsub("{sandboxTransactionId}", "")
+      uri = Ibanity.client.build_uri(path)
+      list_by_uri(uri: uri, query_params: query_params)
+    end
+
+    def self.find(id:, financial_institution_user_id:, financial_institution_id:, financial_institution_account_id:)
+      path = Ibanity.api_schema["sandbox"]["transactions"]
+        .gsub("{financialInstitutionId}", financial_institution_id)
+        .gsub("{financialInstitutionUserId}", financial_institution_user_id)
+        .gsub("{financialInstitutionAccountId}", financial_institution_account_id)
+        .gsub("{sandboxTransactionId}", id)
+      uri = Ibanity.client.build_uri(path)
+      find_by_uri(uri: uri)
+    end
+
+    def self.delete(id:, financial_institution_user_id:, financial_institution_id:, financial_institution_account_id:)
+      path = Ibanity.api_schema["sandbox"]["transactions"]
+        .gsub("{financialInstitutionId}", financial_institution_id)
+        .gsub("{financialInstitutionUserId}", financial_institution_user_id)
+        .gsub("{financialInstitutionAccountId}", financial_institution_account_id)
+        .gsub("{sandboxTransactionId}", id)
+      uri = Ibanity.client.build_uri(path)
+      destroy_by_uri(uri: uri)
+    end
+  end
+end
