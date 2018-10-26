6bb43679e2c338e90c341e0b3b2ccdd4afefd746
Bruno Dusausoy <bruno.dusausoy@ibanity.com>
2018-10-26 21:55:35 +0200

    Add mandatory application customer reference to customer access token creation

--- lib/ibanity/api/customer_access_token.rb
+++ lib/ibanity/api/customer_access_token.rb
@@ -1,8 +1,10 @@
 module Ibanity
   class CustomerAccessToken < ::Ibanity::BaseResource
-    def self.create(idempotency_key: nil, **attributes)
+    def self.create(application_customer_reference, idempotency_key: nil, **attributes)
       path = ::Ibanity.api_schema["customerAccessTokens"]
       uri = ::Ibanity.client.build_uri(path)
+      attributes.merge!(application_customer_reference: application_customer_reference)
+
       create_by_uri(uri: uri, resource_type: "customerAccessToken", attributes: attributes, idempotency_key: idempotency_key)
     end
   end
