490d0876868a9c9e89e67ecd71b89694f5f41b49
Christina Wolfe <christina.wolfe@ibanity.com>
2019-02-27 17:10:18 +0100

    Remove p

--- lib/ibanity/api/isabel_connect/balance.rb
+++ lib/ibanity/api/isabel_connect/balance.rb
@@ -2,7 +2,7 @@ module Ibanity
   module IsabelConnect
     class Balance < Ibanity::BaseResource
       def self.list(account_id:, access_token:, headers: nil, **query_params)
-        p Ibanity.isabel_connect_schema["accounts"]["balances"]
+        Ibanity.isabel_connect_schema["accounts"]["balances"]
         uri = Ibanity.isabel_connect_schema["account"]["balances"].sub("{accountId}", account_id)
         list_by_uri(uri: uri, query_params: query_params, customer_access_token: access_token, headers: headers)
       end
