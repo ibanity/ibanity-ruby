248c5f28901085d5b8c5f047ef0550e2901b7a2a
Damien Bravin <damien.bravin@ibanity.com>
2019-09-27 09:56:26 +0200

    Pass redirect uri when using authorization code.

--- lib/ibanity/api/ponto_connect/token.rb
+++ lib/ibanity/api/ponto_connect/token.rb
@@ -1,15 +1,14 @@
 module Ibanity
   module PontoConnect
     class Token < Ibanity::OAuthResource
-      def self.create(refresh_token: nil, authorization_code: nil, redirect_uri:, idempotency_key: nil, code_verifier: nil)
+      def self.create(refresh_token: nil, authorization_code: nil, redirect_uri: nil, idempotency_key: nil, code_verifier: nil)
         uri = Ibanity.ponto_connect_api_schema["oauth2"]["token"]
         grant_type = refresh_token ? "refresh_token" : "authorization_code"
         arguments = [
           ["grant_type", grant_type],
-          ["client_id", Ibanity.client.ponto_connect_client_id],
-          ["redirect_uri", redirect_uri]
+          ["client_id", Ibanity.client.ponto_connect_client_id]
         ]
-        arguments << ["code", authorization_code] << ["code_verifier", code_verifier] if authorization_code
+        arguments << ["code", authorization_code] << ["code_verifier", code_verifier] << ["redirect_uri", redirect_uri] if authorization_code
         arguments << ["refresh_token", refresh_token] if refresh_token
         payload = URI.encode_www_form(arguments)
         create_by_uri(uri: uri, payload: payload, idempotency_key: idempotency_key, headers: self.headers)
