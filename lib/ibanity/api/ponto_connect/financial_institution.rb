9004e80c64664240ed5532e5ab4fa5cbe064a6e4
Christina Wolfe <christina.wolfe@ibanity.com>
2020-03-27 09:30:29 +0100

    Allow financial institution to be fetched without an access token

--- lib/ibanity/api/ponto_connect/financial_institution.rb
+++ lib/ibanity/api/ponto_connect/financial_institution.rb
@@ -6,7 +6,7 @@ module Ibanity
         list_by_uri(uri: uri, query_params: query_params, customer_access_token: access_token)
       end
 
-      def self.find(access_token:, id:)
+      def self.find(access_token: nil, id:)
         uri = Ibanity.ponto_connect_api_schema["financialInstitutions"].sub("{financialInstitutionId}", id)
         find_by_uri(uri: uri, customer_access_token: access_token)
       end
