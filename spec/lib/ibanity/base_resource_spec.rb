5d7787bc6cb7391f7855505246aa1e982ff685de
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2021-04-07 10:05:02 +0200

    Use simpler examples

--- spec/lib/ibanity/base_resource_spec.rb
+++ spec/lib/ibanity/base_resource_spec.rb
@@ -1,23 +1,24 @@
 require "ibanity"
 
+class Ibanity::Xs2a::Car < Ibanity::BaseResource; end
+class Ibanity::Xs2a::Manufacturer < Ibanity::BaseResource; end
+class Ibanity::Xs2a::Owner < Ibanity::BaseResource; end
+
 RSpec.describe Ibanity::BaseResource do
   describe "#relationship_klass" do
     context "when 'data' attribute is present" do
-      let(:json) { Fixture.load_json("account.json") }
+      let(:json) { Fixture.load_json("car.json") }
 
       it "retrieves the resource name from the 'type' attribute if it is present" do
-        json["relationships"]["fi"] = json["relationships"]["financialInstitution"]
-        json["relationships"].delete("financialInstitution")
+        actual = Ibanity::Xs2a::Car.new(json)
 
-        actual = Ibanity::Xs2a::Account.new(json)
-
-        expect(actual).to respond_to(:financial_institution)
+        expect(actual).to respond_to(:manufacturer)
       end
 
-      it "retrieves the resource name from the 'type' attribute if it is present" do
-        actual = Ibanity::Xs2a::Account.new(json)
+      it "falls back to the element name otherwise" do
+        actual = Ibanity::Xs2a::Car.new(json)
 
-        expect(actual).to respond_to(:financial_institution)
+        expect(actual).to respond_to(:owner)
       end
     end
   end
