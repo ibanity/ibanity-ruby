e6ced5c18d2ae21512d44759605f55cb88c1ce6f
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2021-04-08 16:53:20 +0200

    Refactor tests

--- spec/lib/ibanity/base_resource_spec.rb
+++ spec/lib/ibanity/base_resource_spec.rb
@@ -2,23 +2,23 @@ require "ibanity"
 
 class Ibanity::Xs2a::Car < Ibanity::BaseResource; end
 class Ibanity::Xs2a::Manufacturer < Ibanity::BaseResource; end
-class Ibanity::Xs2a::Owner < Ibanity::BaseResource; end
 
 RSpec.describe Ibanity::BaseResource do
   describe "#relationship_klass" do
     context "when 'data' attribute is present" do
-      let(:json) { Fixture.load_json("car.json") }
-
       it "retrieves the resource name from the 'type' attribute if it is present" do
-        actual = Ibanity::Xs2a::Car.new(json)
+        car = Ibanity::Xs2a::Car.new(Fixture.load_json("relationships/data_with_type.json"))
 
-        expect(actual).to respond_to(:manufacturer)
+        expect(car).to respond_to(:manufacturer)
       end
 
       it "falls back to the element name otherwise" do
-        actual = Ibanity::Xs2a::Car.new(json)
+        car = Ibanity::Xs2a::Car.new(Fixture.load_json("relationships/data_without_type.json"))
 
-        expect(actual).to respond_to(:owner)
+        expect(car).to respond_to(:manufacturer)
+      end
+    end
+
       end
     end
   end
