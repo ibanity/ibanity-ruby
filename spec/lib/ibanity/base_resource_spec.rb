f6d19d6e388b43e49cc8740d6b0184fdca144eca
Bruno Dusausoy <bruno.dusausoy@ibanity.com>
2021-05-04 10:28:39 +0200

    Simplify tests

--- spec/lib/ibanity/base_resource_spec.rb
+++ spec/lib/ibanity/base_resource_spec.rb
@@ -5,23 +5,17 @@ class Ibanity::Xs2a::Manufacturer < Ibanity::BaseResource; end
 
 RSpec.describe Ibanity::BaseResource do
   describe "#relationship_klass" do
-    before do
-      allow(Ibanity::Xs2a::Manufacturer).to receive(:find_by_uri) { true }
-    end
-
     context "when 'data' attribute is present" do
       it "retrieves the resource name from the 'type' attribute if it is present" do
         car = Ibanity::Xs2a::Car.new(Fixture.load_json("relationships/data_with_type.json"))
 
         expect(car).to respond_to(:maker)
-        expect(car.maker).to be(true)
       end
 
       it "falls back to the element name otherwise" do
         car = Ibanity::Xs2a::Car.new(Fixture.load_json("relationships/data_without_type.json"))
 
         expect(car).to respond_to(:manufacturer)
-        expect(car.manufacturer).to be(true)
       end
     end
 
@@ -30,7 +24,6 @@ RSpec.describe Ibanity::BaseResource do
         car = Ibanity::Xs2a::Car.new(Fixture.load_json("relationships/meta_with_type.json"))
   
         expect(car).to respond_to(:manufacturer)
-        expect(car.manufacturer).to be(true)
       end
     end
 
