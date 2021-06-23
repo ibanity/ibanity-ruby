1311eacf3e716e6e78abe8b83501143712856dd7
Christophe Leclef <christophe.leclef@ibanity.com>
2021-06-23 14:35:00 +0200

    get updated transaction via transaction model

--- lib/ibanity/api/xs2a/transaction.rb
+++ lib/ibanity/api/xs2a/transaction.rb
@@ -1,11 +1,17 @@
 module Ibanity
   module Xs2a
     class Transaction < Ibanity::BaseResource
-      def self.list(financial_institution_id:, account_id:, customer_access_token:, headers: nil,  **query_params)
-        uri = Ibanity.xs2a_api_schema["customer"]["financialInstitution"]["transactions"]
-          .sub("{financialInstitutionId}", financial_institution_id)
-          .sub("{accountId}", account_id)
-          .sub("{transactionId}", "")
+      def self.list(financial_institution_id:, account_id:, synchronization_id:, customer_access_token:, headers: nil,  **query_params)
+        uri = if synchronization_id
+          Ibanity.xs2a_api_schema["customer"]["synchronization"]["updatedTransactions"]
+          .sub("{synchronizationId}", synchronization_id)
+          .sub("{updateTransactionId}", "")
+        else
+          Ibanity.xs2a_api_schema["customer"]["financialInstitution"]["transactions"]
+            .sub("{financialInstitutionId}", financial_institution_id)
+            .sub("{accountId}", account_id)
+            .sub("{transactionId}", "")
+        end
         list_by_uri(uri: uri, query_params: query_params, customer_access_token: customer_access_token, headers: headers)
       end
 
