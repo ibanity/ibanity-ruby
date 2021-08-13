f55ce1137b6224ecacb9a6e3f6f7246ffcf0b122
Christina Wolfe <christina.wolfe@ibanity.com>
2021-08-13 15:16:58 +0200

    Add update financial institution transaction method

--- lib/ibanity/api/ponto_connect/sandbox/financial_institution_transaction.rb
+++ lib/ibanity/api/ponto_connect/sandbox/financial_institution_transaction.rb
@@ -25,6 +25,14 @@ module Ibanity
             .sub("{financialInstitutionTransactionId}", "")
           create_by_uri(uri: uri, resource_type: "financialInstitutionTransaction", attributes: attributes, customer_access_token: access_token)
         end
+
+        def self.update(access_token:, id:, financial_institution_id:, financial_institution_account_id:, **attributes)
+          uri = Ibanity.ponto_connect_api_schema["sandbox"]["financialInstitution"]["financialInstitutionAccount"]["financialInstitutionTransactions"]
+            .sub("{financialInstitutionId}", financial_institution_id)
+            .sub("{financialInstitutionAccountId}", financial_institution_account_id)
+            .sub("{financialInstitutionTransactionId}", id)
+          update_by_uri(uri: uri, resource_type: "financialInstitutionTransaction", attributes: attributes, customer_access_token: access_token)
+        end
       end
     end
   end
