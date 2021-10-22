29ea888492815d310ac488c751d9c6182725d14e
Bram Jans <ik@bramjans.com>
2021-10-22 16:03:17 +0200

    Make access_token required for ReauthorizationRequest create

--- lib/ibanity/api/ponto_connect/reauthorization_request.rb
+++ lib/ibanity/api/ponto_connect/reauthorization_request.rb
@@ -1,7 +1,7 @@
 module Ibanity
   module PontoConnect
     class ReauthorizationRequest < Ibanity::BaseResource
-      def self.create(account_id:, access_token: nil, **attributes)
+      def self.create(account_id:, access_token:, **attributes)
         uri = Ibanity.ponto_connect_api_schema["account"]["reauthorizationRequests"].gsub("{accountId}", account_id)
         create_by_uri(uri: uri, resource_type: "reauthorizationRequest", attributes: attributes, customer_access_token: access_token)
       end
