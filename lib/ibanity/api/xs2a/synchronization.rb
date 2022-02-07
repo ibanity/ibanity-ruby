2a224543fac632cbbc7c4f3e3b39f3e9d57680cc
Christophe Leclef <christophe.leclef@ibanity.com>
2022-02-07 14:03:41 +0100

    add synchro list method

--- lib/ibanity/api/xs2a/synchronization.rb
+++ lib/ibanity/api/xs2a/synchronization.rb
@@ -1,6 +1,12 @@
 module Ibanity
   module Xs2a
     class Synchronization < Ibanity::BaseResource
+      def self.list(account_information_access_request_id:, customer_access_token:, headers: nil,  **query_params)
+        Ibanity.xs2a_api_schema["customer"]["accountInformationAccessRequests"]["initialAccountTransactionsSynchronizations"]
+        .sub("{accountInformationAccessRequestId}", account_information_access_request_id)
+        list_by_uri(uri: uri, query_params: query_params, customer_access_token: customer_access_token, headers: headers)
+      end
+
       def self.create(customer_access_token:, **attributes)
         uri = Ibanity.xs2a_api_schema["customer"]["synchronizations"]
           .sub("{synchronizationId}", "")
