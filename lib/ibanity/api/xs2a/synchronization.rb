2e27938aba4b378ba8f4bd4081426133112e67ea
Christophe Leclef <christophe.leclef@ibanity.com>
2022-02-07 14:30:59 +0100

    initialAccountTransactionsSynchronizations path

--- lib/ibanity/api/xs2a/synchronization.rb
+++ lib/ibanity/api/xs2a/synchronization.rb
@@ -2,7 +2,7 @@ module Ibanity
   module Xs2a
     class Synchronization < Ibanity::BaseResource
       def self.list(account_information_access_request_id:, customer_access_token:, headers: nil,  **query_params)
-        Ibanity.xs2a_api_schema["customer"]["accountInformationAccessRequests"]["initialAccountTransactionsSynchronizations"]
+        Ibanity.xs2a_api_schema["customer"]["financialInstitution"]["accountInformationAccessRequests"]["initialAccountTransactionsSynchronizations"]
         .sub("{accountInformationAccessRequestId}", account_information_access_request_id)
         list_by_uri(uri: uri, query_params: query_params, customer_access_token: customer_access_token, headers: headers)
       end
