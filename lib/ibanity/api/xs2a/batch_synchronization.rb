8d92412ec12afd51a6a139343a8a441fe92caf2c
Christophe Leclef <christophe.leclef@ibanity.com>
2022-02-17 13:35:57 +0100

    add batch synchronizations

---
+++ lib/ibanity/api/xs2a/batch_synchronization.rb
@@ -0,0 +1,11 @@
+module Ibanity
+  module Xs2a
+    class BatchSynchronization < Ibanity::BaseResource
+      def self.create(idempotency_key: nil, **attributes)
+        path = Ibanity.sandbox_api_schema["batchSynchronization"].gsub("{batchSynchronizationId}", "")
+        uri = Ibanity.client.build_uri(path)
+        create_by_uri(uri: uri, resource_type: "batchSynchronization", attributes: attributes, idempotency_key: idempotency_key)
+      end
+    end
+  end
+end
