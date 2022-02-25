914f5494a8effa9872670d9f7fa6a3b61bf67ea7
Christophe Leclef <christophe.leclef@ibanity.com>
2022-02-25 10:20:48 +0100

    incorrect schema key name

--- lib/ibanity/api/xs2a/batch_synchronization.rb
+++ lib/ibanity/api/xs2a/batch_synchronization.rb
@@ -2,7 +2,7 @@ module Ibanity
   module Xs2a
     class BatchSynchronization < Ibanity::BaseResource
       def self.create(idempotency_key: nil, **attributes)
-        path = Ibanity.xs2a_api_schema["batchSynchronization"].gsub("{batchSynchronizationId}", "")
+        path = Ibanity.xs2a_api_schema["batchSynchronizations"].gsub("{batchSynchronizationId}", "")
         uri = Ibanity.client.build_uri(path)
         create_by_uri(uri: uri, resource_type: "batchSynchronization", attributes: attributes, idempotency_key: idempotency_key)
       end
