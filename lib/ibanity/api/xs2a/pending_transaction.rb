a85b3e1fe9d7cfde58c1eb1045b1b0aeb5049e0f
Christophe Leclef <christophe.leclef@ibanity.com>
2023-04-05 10:38:56 +0200

    fix request for updated pending transactions

--- lib/ibanity/api/xs2a/pending_transaction.rb
+++ lib/ibanity/api/xs2a/pending_transaction.rb
@@ -2,10 +2,16 @@ module Ibanity
   module Xs2a
     class PendingTransaction < Ibanity::BaseResource
       def self.list(financial_institution_id: nil, account_id: nil, synchronization_id: nil, customer_access_token:, headers: nil,  **query_params)
-        uri = Ibanity.xs2a_api_schema["customer"]["financialInstitution"]["pendingTransactions"]
+        uri = if synchronization_id
+          Ibanity.xs2a_api_schema["customer"]["synchronization"]["updatedPendingTransactions"]
+          .sub("{synchronizationId}", synchronization_id)
+        else
+          Ibanity.xs2a_api_schema["customer"]["financialInstitution"]["pendingTransactions"]
             .sub("{financialInstitutionId}", financial_institution_id)
             .sub("{accountId}", account_id)
             .sub("{pendingTransactionId}", "")
+        end
+
         list_by_uri(uri: uri, query_params: query_params, customer_access_token: customer_access_token, headers: headers)
       end
 
