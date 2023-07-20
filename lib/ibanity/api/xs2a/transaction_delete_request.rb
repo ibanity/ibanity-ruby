cfd72466f7b22b4af81f2564d1b61683d771ab81
Christophe Leclef <christophe.leclef@ibanity.com>
2023-07-20 18:17:40 +0200

    add missing customer_access_token in calls

--- lib/ibanity/api/xs2a/transaction_delete_request.rb
+++ lib/ibanity/api/xs2a/transaction_delete_request.rb
@@ -10,7 +10,7 @@ module Ibanity
       def self.create_for_customer(idempotency_key: nil, customer_access_token:, **attributes)
         path = Ibanity.xs2a_api_schema["customer"]["transactionDeleteRequests"]
         uri = Ibanity.client.build_uri(path)
-        create_by_uri(uri: uri, resource_type: "transactionDeleteRequest", attributes: attributes, idempotency_key: idempotency_key)
+        create_by_uri(uri: uri, resource_type: "transactionDeleteRequest", attributes: attributes, idempotency_key: idempotency_key, customer_access_token: customer_access_token)
       end
 
       def self.create_for_financial_institution_and_account(idempotency_key: nil, financial_institution_id:, account_id:, customer_access_token:, **attributes)
@@ -18,7 +18,7 @@ module Ibanity
                     .sub("{financialInstitutionId}", financial_institution_id)
                     .sub("{accountId}", account_id)
         uri = Ibanity.client.build_uri(path)
-        create_by_uri(uri: uri, resource_type: "transactionDeleteRequest", attributes: attributes, idempotency_key: idempotency_key)
+        create_by_uri(uri: uri, resource_type: "transactionDeleteRequest", attributes: attributes, idempotency_key: idempotency_key, customer_access_token: customer_access_token)
       end
     end
   end
